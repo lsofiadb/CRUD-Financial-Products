b6d8c240340545213cc1fb93e9506942062bf1d4
Laura Sofia Dueñas Bulla <lauraduenas1105@gmail.com>
2022-09-28 16:11:02 -0500

    initial configuration

---
+++ src/main/java/CRUDFinancialProducts/CrudFinancialProductsApplication.java
@@ -0,0 +1,13 @@
+package CRUDFinancialProducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudFinancialProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudFinancialProductsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/CRUDFinancialProducts/CrudFinancialProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package CRUDFinancialProducts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudFinancialProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
